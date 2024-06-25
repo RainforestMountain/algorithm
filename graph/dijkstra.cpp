59552d408162e60d0ae9c35213ced128e5e8f405
rain-falling-cold-s <3521748889@qq.com>
2024-06-25 23:46:55 +0800

    图

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<int> graph;
+
+int main()
+{
+    
+    unordered_map<int, int> map;
+    map.insert({1, 2});
+    vector<int> v;
+    
+    cout << "你好" << endl;
+    int n;
+    cin >> n;
+    cout << n <<"\n";
+    return 0;
+}
