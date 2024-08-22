c983d5c141b58b760d84de8fcf1f6c7fc32a4e98
rain-falling-cold-s <3521748889@qq.com>
2024-08-22 23:51:38 +0800

    代码提交

---
+++ Demo_8_20/src/Thread1.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.lang.Integer.MAX_VALUE;
+
+public class Thread1 {
+    public static void main(String[] args) {
+        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor();
+    }
+
+}
