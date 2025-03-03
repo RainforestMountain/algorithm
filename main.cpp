7c18959087dfd90630501b75ceda3ab333fea541
TemporayRain <147801852+RainforestMountain@users.noreply.github.com>
2025-03-03 23:51:00 +0800

    二叉树

--- main.cpp
+++ main.cpp
@@ -3,23 +3,50 @@
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    queue<string> vc;
-    set<string> st;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        if (st.find(s) == st.end()) {
-            vc.push(s);
-            st.insert(s);
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        set<TreeNode *> p_nodes;
+        set<TreeNode *> q_nodes;
+        getPath(root, p, p_nodes);
+        getPath(root, q, q_nodes);
+        for (auto it = q_nodes.end(); it != q_nodes.begin(); it--) {
+            if (p_nodes.find(*it) != p_nodes.end()) {
+                return *it;
+            }
         }
+        if (p_nodes.find(*q_nodes.begin()) != p_nodes.end()) {
+            return *q_nodes.begin();
+        }
+        return nullptr;
+    };
+
+    bool getPath(TreeNode *root, TreeNode *&p, set<TreeNode *> &nodes) {
+        if (root == nullptr) {
+            return false;
+        }
+        if (root == p) {
+            nodes.insert(root);
+            return true;
+        }
+        bool ans = getPath(root->left, p, nodes);
+        ans = ans | getPath(root->right, p, nodes);
+        if (ans) {
+            nodes.insert(root);
+        }
+        return ans;
     }
-    while (!vc.empty()) {
-        cout << vc.front() << "\n";
-        vc.pop();
-    }
+};
+
+int main() {
+
     return 0;
 }
