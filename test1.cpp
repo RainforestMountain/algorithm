f7216462f90222f120c73620494cdcfbaf7ef0b2
TemporayRain <147801852+RainforestMountain@users.noreply.github.com>
2025-03-09 23:42:18 +0800

    算法代码

--- test1.cpp
+++ test1.cpp
@@ -2,40 +2,107 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-const int N = 200000 + 4;
 
-void solve() {
-    int n;
-    cin >> n;
-    unordered_set<string> s;
-    for (int i = 1; i <= n; i++) {
-        int m;
-        cin >> m;
-        bool jude = false;
-        for (int j = 1; j <= m; j++) {
-            string src;
-            cin >> src;
-            if (src.find("bie") != std::string::npos && s.find(src) == s.end()) {
-                s.insert(src);
-                jude = true;
-                cout << src << "\n";
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int countUnivalSubtrees(TreeNode *root) {
+        dfs(root);
+        return result;
+    }
+
+    int result = 0;
+
+    bool dfs(TreeNode *root) {
+        if (root == nullptr) {
+            return true;
+        }
+        bool left = dfs(root->left);
+        bool right = dfs(root->right);
+        bool ans = false;
+        if (left && right) {
+            if (root->left != nullptr && root->right != nullptr) {
+                if (root->val == root->left->val && root->val == root->right->val) {
+                    ans = true;
+                    result++;
+                }
+            } else if (root->left != nullptr && root->right == nullptr) {
+                if (root->val == root->left->val) {
+                    ans = true;
+                    result++;
+                }
+            } else if (root->left == nullptr && root->right != nullptr) {
+                if (root->val == root->right->val) {
+                    ans = true;
+                    result++;
+                }
+            } else {
+                ans = true;
+                result++;
             }
         }
-        if (!jude) {
-            cout << "Time to play Genshin Impact, Teacher Rice!" << "\n";
-        }
+        return ans;
     }
 
-}
+    int closestValue(TreeNode *root, double target) {
+        vector<int> nodes;
+        backtracking(root, nodes);
+        int det = 0x3f3f3f3f;
+        int ops = -1;
+        for (int i = 0; i < nodes.size(); i++) {
+            if (abs(target - nodes[i]) < det) {
+                det = abs(target - nodes[i]);
+                ops = i;
+            }
+        }
+        return nodes[ops];
+    }
+
+    void backtracking(TreeNode *root, vector<int> &nodes) {
+        if (root == nullptr) {
+            return;
+        }
+        backtracking(root, nodes);
+        nodes.push_back(root->val);
+        backtracking(root, nodes);
+    }
+};
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode *root) { //前序加中序
+
+    }
+
+    void preOrder(TreeNode *root, string &path) {
+        if (root == nullptr) {
+            return;
+        }
+        path.push_back( )
+        preOrder(root->left, path);
+        preOrder(root->right, path);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode *deserialize(string data) {
+
+    }
+};
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
-    int t = 1;
-//    cin >> t;
-    while (t--) {
-        solve();
-    }
     return 0;
 }
